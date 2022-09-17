fb093aeb4c628e9c045288a4c701624a96ed6893
Shey Njila <sheymengla.devtest@gmail.com>
2022-09-17 19:02:29 -0400

    troubleshoting apply

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ module "vpc" {
 module "ec2" {
   source         = "./ec2"
   public_subnets = module.vpc.public_subnets
-  instance_type  = "t3.micro"
-  key_name       = "MyKey"
+  instance_type  = "t2.micro"
+  key_name       = "MyKeyPair"
   user_data      = filebase64("./userdata.tpl")
   vpc_id         = module.vpc.vpc_id
 }
